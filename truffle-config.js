961bc0a4e57bd7715663e2b6046b8cc73cb9b90a
abhishekbh0476 <115627951+abhishekbh0476@users.noreply.github.com>
2025-03-21 14:00:18 +0530

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,31 @@
+
+
+module.exports = {
+ 
+
+  contracts_build_directory: './client/src/artifacts',
+  networks: {
+   
+    development: {
+      host: "127.0.0.1",    
+      port: 7545,           
+      network_id: "5777",     
+    },
+   
+  },
+
+  mocha: {
+  },
+
+  compilers: {
+    solc: {
+      
+    }
+  },
+
+
+
+  db: {
+    enabled: false
+  }
+};
